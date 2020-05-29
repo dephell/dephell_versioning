ff9c3169a445b1a1dc462838a7b2a9949c72f991
Viginox <31762120+Viginox@users.noreply.github.com>
2020-05-29 23:05:28 +0900

    Fix recommendations from PR discussion
    * Change `alpha` to `a` and `beta` to `b` in version strings
    * Revert default pre version to `rc`
    * Minor code cleanup

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -12,30 +12,30 @@ from dephell_versioning import bump_version
     ('patch',    '1.2.3', '1.2.4'),
 
     # pre-base version
-    ('premajor', '1.2.3', '2.0.0alpha1'),
-    ('preminor', '1.2.3', '1.3.0alpha1'),
-    ('prepatch', '1.2.3', '1.2.4alpha1'),
+    ('premajor', '1.2.3', '2.0.0rc1'),
+    ('preminor', '1.2.3', '1.3.0rc1'),
+    ('prepatch', '1.2.3', '1.2.4rc1'),
 
     # add pep specific parts
-    ('pre',      '1.2.3', '1.2.3alpha1'), # pre will now append the first value in ('alpha', 'beta', 'rc')
-    ('alpha',    '1.2.3', '1.2.3alpha1'),
-    ('beta',     '1.2.3', '1.2.3beta1'),
+    ('pre',      '1.2.3', '1.2.3rc1'),
+    ('alpha',    '1.2.3', '1.2.3a1'),
+    ('beta',     '1.2.3', '1.2.3b1'),
     ('rc',       '1.2.3', '1.2.3rc1'),
 
     # update alpha -> beta -> rc
-    ('beta',     '1.2.3alpha1', '1.2.3beta1'),
-    ('rc',       '1.2.3beta1',  '1.2.3rc1'),
+    ('beta',     '1.2.3a1', '1.2.3b1'),
+    ('rc',       '1.2.3b1', '1.2.3rc1'),
 
     # bump alpha, beta, rc
-    ('alpha',    '1.2.3alpha1', '1.2.3alpha2'),
-    ('pre',      '1.2.3alpha1', '1.2.3alpha2'),
-    ('beta',     '1.2.3beta1',  '1.2.3beta2'),
-    ('pre',      '1.2.3beta1',  '1.2.3beta2'),
-    ('rc',       '1.2.3rc1',    '1.2.3rc2'),
-    ('pre',      '1.2.3rc1',    '1.2.3rc2'),
+    ('alpha',    '1.2.3a1',  '1.2.3a2'),
+    ('pre',      '1.2.3a1',  '1.2.3a2'),
+    ('beta',     '1.2.3b1',  '1.2.3b2'),
+    ('pre',      '1.2.3b1',  '1.2.3b2'),
+    ('rc',       '1.2.3rc1', '1.2.3rc2'),
+    ('pre',      '1.2.3rc1', '1.2.3rc2'),
 
     # add special part
-    #('pre',      '1.2.3', '1.2.3rc1'), # disabled because of changes in bump_pre implementation
+    ('pre',      '1.2.3', '1.2.3rc1'),
     ('post',     '1.2.3', '1.2.3.post1'),
     ('dev',      '1.2.3', '1.2.3.dev1'),
     ('local',    '1.2.3', '1.2.3+1'),
@@ -54,4 +54,5 @@ from dephell_versioning import bump_version
 
 ])
 def test_bump_version(rule, old, new):
-    assert bump_version(rule=rule, version=old, scheme='pep') == new, "Rule {} failed for '{}'!".format(rule, old)
+    assert bump_version(rule=rule, version=old, scheme='pep') == new,\
+        "Rule {} failed for '{}'!".format(rule, old)
