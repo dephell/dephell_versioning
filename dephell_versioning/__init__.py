df174389d90cca6bec96641d7b6f7eced8200f38
Gram <master_fess@mail.ru>
2020-05-30 07:23:07 +0200

    bump version to 0.1.2

--- dephell_versioning/__init__.py
+++ dephell_versioning/__init__.py
@@ -2,7 +2,7 @@ from ._core import bump_file, bump_version, get_aliases, get_rules, get_schemes
 from ._schemes import BaseScheme, SCHEMES
 
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 __all__ = [
